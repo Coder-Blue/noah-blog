85a69481badf995c61c86ea1e8a88756ab60d46c
Coder-Blue <trananhquan1009@gmail.com>
2025-03-31 19:29:30 +0700

    31/03/25 - Prettier Formatted

--- src/app/[locale]/(blog)/blog/(others)/rss/route.ts
+++ src/app/[locale]/(blog)/blog/(others)/rss/route.ts
@@ -27,7 +27,7 @@ export async function GET() {
     .join("/n");
 
   const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
-        <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+        <rss version="2.0">
             <channel>
                 <title>${t("defaultTitle")}</title>
                 <link>${baseUrl(t("localeFormat"))}/blog</link>
